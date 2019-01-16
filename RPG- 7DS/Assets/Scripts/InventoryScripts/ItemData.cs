9aff2c60eeaffd818494a49b538fb456a8692ec4
Jake Meaker <JM219785@falmouth.ac.uk>
2019-01-16 15:57:41 +0000

    .

---
+++ RPG- 7DS/Assets/Scripts/InventoryScripts/ItemData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class ItemData : ScriptableObject {
+
+    
+    public GameObject[] examinableItems;
+    new public string name = "New Item";
+    public Sprite icon = null;
+    public GameObject itemPrefab;
+    public GameObject itemPrefabExam;
+    
+    public virtual void Use()
+    {
+        //use the item
+        //something might happen
+
+        Debug.Log("Using " + name); 
+    }
+
+
+      
+
+    
+}
